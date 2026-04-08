fa77ea842955e8705bc439ae0cfd30ee3627da6a
laran <quintanarijoan@gmail.com>
2026-04-08 19:23:01 -0300

    style: estilo personalizado de navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <!-- Componente del navbar -->
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class=" navbar navbar-expand-lg  navbar-plomada">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">La plomada</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
