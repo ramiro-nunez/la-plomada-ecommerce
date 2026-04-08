8056562738641e6cabc176719c3b7bc78e391a7b
laran <quintanarijoan@gmail.com>
2026-04-08 16:56:23 -0300

    feat: banner de bienvenida + alerta seccion en construccion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>La Plomada</title>
-    <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css')}}">
-
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>La Plomada</title>
+  <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css')}}">
 </head>
+
 <body>
-    <!-- Componente del navbar -->
+  <!-- Componente del navbar -->
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">La plomada</a>
@@ -17,16 +17,62 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link " href="/quienes-somos">Quienes Somos</a>
-        <a class="nav-link" href="/comercializacion">Comercializacion</a>
-        <a class="nav-link" href="/terminos-y-condiciones">Terminos y condiciones</a>
-        <a class="nav-link" href="/contacto">Contacto</a>
+        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#en-construccion">Quienes Somos</a>
+        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#en-construccion">Comercializacion</a>
+        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#en-construccion">Terminos y condiciones</a>
+        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#en-construccion">Contacto</a>
       </div>
     </div>
   </div>
 </nav>
-    <!-- Fin Navbar -->
-<div class="container text-center">Bienvenido</div>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <!-- Fin Navbar -->
+<!-- modal de pagina en construcción -->
+<div class="modal fade" id="en-construccion" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Sección en Construcción</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Disculpe las molestias, pronto estará disponible! :)
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success" data-bs-dismiss="modal" aria-label="Close">Entendido</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- fin modal -->
+
+<div class="container text-center">
+  <p>Bienvenido</p>
+</div>
+<!-- Carrousel de bienvenida -->
+  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="{{ asset('img/banners/bann3.png')}}" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="{{ asset('img/banners/bann1.png')}}" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="{{ asset('img/banners/bann2.png')}}" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div> 
+  <!-- Fin Carrousel -->
+
+
+  <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
