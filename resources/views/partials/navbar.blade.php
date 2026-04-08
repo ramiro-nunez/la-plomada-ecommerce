4f4baafe8067d218a407922dcd0b6f01b3350ad1
laran <quintanarijoan@gmail.com>
2026-04-08 17:43:07 -0300

    refactor: separando en componentes

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,36 @@
+<!-- Componente del navbar -->
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">La plomada</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#en-construccion">Quienes Somos</a>
+        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#en-construccion">Comercializacion</a>
+        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#en-construccion">Terminos y condiciones</a>
+        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#en-construccion">Contacto</a>
+      </div>
+    </div>
+  </div>
+</nav>
+  <!-- Fin Navbar -->
+<!-- modal de pagina en construcción -->
+<div class="modal fade" id="en-construccion" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Sección en Construcción</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Disculpe las molestias, pronto estará disponible! :)
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success" data-bs-dismiss="modal" aria-label="Close">Entendido</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- fin modal -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,50 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>La Plomada</title>
-  <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css')}}">
-</head>
+@extends('app')
+@section('title', 'Inicio')
 
-<body>
-  <!-- Componente del navbar -->
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">La plomada</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#en-construccion">Quienes Somos</a>
-        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#en-construccion">Comercializacion</a>
-        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#en-construccion">Terminos y condiciones</a>
-        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#en-construccion">Contacto</a>
-      </div>
-    </div>
-  </div>
-</nav>
-  <!-- Fin Navbar -->
-<!-- modal de pagina en construcción -->
-<div class="modal fade" id="en-construccion" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Sección en Construcción</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Disculpe las molestias, pronto estará disponible! :)
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-success" data-bs-dismiss="modal" aria-label="Close">Entendido</button>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- fin modal -->
-
+@section('content')
 <div class="container text-center">
   <p>Bienvenido</p>
 </div>
@@ -71,8 +28,6 @@
     </button>
   </div> 
   <!-- Fin Carrousel -->
+@endsection
 
-
-  <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-</body>
-</html>
+  
