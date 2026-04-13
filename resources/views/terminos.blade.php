5daf7517ef879fb8c2fe2870e8ab8c22961ea03b
Ramiro Núñez <ramirosebastiann@gmail.com>
2026-04-13 10:35:06 -0300

    ajuste display en encabezados y tabla en /comercio

--- resources/views/terminos.blade.php
+++ resources/views/terminos.blade.php
@@ -2,10 +2,8 @@
 @section('title', 'Comercialización')
 
 @section('content')
-    <div class="masthead" style="background-image: url('{{ asset('img/terminos/terminos.png') }}')">
-        <div class="container">
-            <h1 class="titulo-seccion text-center">TÉRMINOS Y CONDICIONES</h1>
-        </div>
+    <div class="encabezado-terms d-flex align-items-center justify-content-center">
+        <h1 class="fw-bold text-white display-3 text-center">TÉRMINOS Y CONDICIONES</h1>
     </div>
     <div class="fondo py-5">
         <div class="container py-4">
