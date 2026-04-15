a12b552ed2ea2fdcb1b35bce641b5f2df4da3f2d
laran <quintanarijoan@gmail.com>
2026-04-14 21:35:08 -0300

    feat: fprmulario de registro

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,10 +7,6 @@
   <title>La Plomada - @yield('title')</title>
   <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css')}}">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
-
-
-
 </head>
 
 <body class="d-flex flex-column min-vh-100">
