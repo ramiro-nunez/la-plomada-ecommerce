25412f272d190c6db0ab041568a2e9a15902b5c6
Ramiro Núñez <ramirosebastiann@gmail.com>
2026-04-25 21:40:28 -0300

    se añadieron los cambios de las ramas de antonio y ramiro

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -11,35 +11,35 @@
     <section class="mt-0" style="background-color: #2D4030;">
     <div class="container py-5">
         <h2 class="text-center subtitulo-seccion text-white fw-bold mb-5">Reeles</h2>
-        <div class="row row-cols-3 justify-content-evenly align-items-center g-4">
+        <div class="row justify-content-center align-items-center g-4">
             <!-- card reel1 -->
             <div class="col-sm-3">
-                <div class="shadow-sm text-center p-2 product-card">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
+                <div class=" text-center product-card-dark">
+                    <div class="mx-auto mb-1 overflow-hidden product-img">
                         <img src="{{ asset('img/catalogo/reel1.jpg') }}" class="w-100 h-100 ">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$85.000</h4>
-                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0">Reel Frontal Daiwa Sweepfire 4000</h5>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Reel Frontal Daiwa Sweepfire 4000</h5>
                 </div>
             </div>
             <!-- card reel2 -->
             <div class="col-sm-3">
-                <div class="shadow-sm text-center p-2 product-card">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
+                <div class=" text-center product-card-dark">
+                    <div class="mx-auto mb-1 overflow-hidden product-img">
                         <img src="{{ asset('img/catalogo/reel2.jpg') }}" class="w-100 h-100 ">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$195.000</h4>
-                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0">Reel Rotativo Abu Garcia Black Max</h5>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Reel Rotativo Abu Garcia Black Max</h5>
                 </div>
             </div>
             <!-- card reel3 -->
             <div class="col-sm-3">
-                <div class="shadow-sm text-center p-2 product-card">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
+                <div class=" text-center product-card-dark">
+                    <div class="mx-auto mb-1 overflow-hidden product-img">
                         <img src="{{ asset('img/catalogo/reel3.jpg') }}" class="w-100 h-100 ">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$280.000</h4>
-                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0">Reel Rotativo Penn Squall Level Wind</h5>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Reel Rotativo Penn Squall Level Wind</h5>
                 </div>
             </div>  
         </div>
@@ -49,38 +49,35 @@
     <section class=" my-5">
     <div class="container">
         <h2 class="text-center subtitulo-seccion fw-bold mb-5">Cañas</h2>
-        <div class="row row-cols-3 justify-content-evenly align-items-center g-4">
+        <div class="row justify-content-center align-items-center g-4">
             <!-- card caña1 -->
             <div class="col-sm-3">
-                <div class="border shadow-sm text-center p-2 product-card">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
+                <div class="border text-center product-card">
+                    <div class="mx-auto mb-1 overflow-hidden product-img">
                         <img src="{{ asset('img/catalogo/cana1.jpg') }}" class="w-100 h-100 object-fit-cover">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$145.000</h4>
-                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0">Caña Telescópica Shimano FX (3.00m)</h5>
-                    <!-- <p class="text-muted small mb-4">
-                        Caña telescópica de fibra de vidrio reforzada. Ideal para la pesca de pejerrey o variada de río. Su diseño compacto la hace perfecta para transportar en el baúl del auto. Acción media y pasahilos de óxido de aluminio.
-                    </p> -->
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Caña Telescópica Shimano FX (3.00m)</h5>
                 </div>
             </div>
             <!-- card caña2 -->
             <div class="col-sm-3">
-                <div class="border shadow-sm text-center p-2 product-card">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
+                <div class="border text-center product-card">
+                    <div class="mx-auto mb-1 overflow-hidden product-img">
                         <img src="{{ asset('img/catalogo/cana2.jpg') }}" class="w-100 h-100 ">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$210.000</h4>
-                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0">Caña Baitcasting Marine Sports Titan (1.98m)</h5>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Caña Baitcasting Marine Sports Titan (1.98m)</h5>
                 </div>
             </div>
             <!-- card caña3 -->
             <div class="col-sm-3">
-                <div class="border shadow-sm text-center p-2 product-card">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
+                <div class="border text-center product-card">
+                    <div class="mx-auto mb-1 overflow-hidden product-img" >
                         <img src="{{ asset('img/catalogo/cana3.jpg') }}" class="w-100 h-100 ">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$185.000</h4>
-                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0">Caña de Mosca Waterdog Fly Series (#5)</h5>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Caña de Mosca Waterdog Fly Series (#5)</h5>
                 </div>
             </div>  
         </div>
@@ -90,67 +87,67 @@
     <section class=" " style="background-color: #1B263B;">
     <div class="container py-5">
         <h2 class="text-center text-white subtitulo-seccion fw-bold mb-5">Accesorios y Herramientas</h2>
-        <div class="row row-cols-3 justify-content-evenly align-items-center g-4">
+        <div class="row justify-content-center align-items-center g-4">
             <!-- card herramienta1 -->
             <div class="col-sm-3">
-                <div class="shadow-sm text-center p-2 product-card">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
+                <div class="text-center product-card-dark">
+                    <div class="mx-auto mb-1 overflow-hidden product-img" >
                         <img src="{{ asset('img/catalogo/señuelo1.jpg') }}" class="w-100 h-100 object-fit-cover">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$32.000</h4>
-                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0">Señuelo Rapala Super Shad Rap (14cm)</h5>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Señuelo Rapala Super Shad Rap (14cm)</h5>
                 </div>
             </div>
             <!-- card herramienta2 -->
             <div class="col-sm-3">
-                <div class="shadow-sm text-center p-2 product-card">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
-                        <img src="{{ asset('img/catalogo/señuelo2.jpg') }}" class="w-100 h-100 ">
+                <div class="text-center product-card-dark">
+                    <div class="mx-auto mb-1 overflow-hidden product-img">
+                        <img src="{{ asset('img/catalogo/señuelo2.jpg') }}" class="w-100 h-100 object-fit-cover">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$12.500</h4>
-                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0">Señuelo Cuchara Giratoria Mepps Aglia (N°4)</h5>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Señuelo Cuchara Giratoria Mepps Aglia (N°4)</h5>
                 </div>
             </div>
             <!-- card herramienta3 -->
             <div class="col-sm-3">
-                <div class="shadow-sm text-center p-2 product-card">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
-                        <img src="{{ asset('img/catalogo/hilo.jpg') }}" class="w-100 h-100 ">
+                <div class="text-center product-card-dark">
+                    <div class="mx-auto mb-1 overflow-hidden product-img" >
+                        <img src="{{ asset('img/catalogo/hilo.jpg') }}" class="w-100 h-100 object-fit-cover">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$48.000</h4>
-                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0">Hilo Multifilamento Sufix 832 (Rollo 150m)</h5>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Hilo Multifilamento Sufix 832 (Rollo 150m)</h5>
                 </div>
             </div>  
         </div>
-        <div class="row row-cols-3 justify-content-evenly align-items-center g-4 mt-1">
+        <div class="row justify-content-center align-items-center g-4 mt-1">
             <!-- card herramienta4 -->
             <div class="col-sm-3">
-                <div class="shadow-sm text-center p-2 product-card">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
+                <div class="text-center product-card-dark">
+                    <div class="mx-auto mb-1 overflow-hidden product-img" >
                         <img src="{{ asset('img/catalogo/anzuelo.jpg') }}" class="w-100 h-100 object-fit-cover">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$15.000</h4>
-                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0">Caja de Anzuelos Mustad Serie 92247 (x50 unidades)</h5>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Caja de Anzuelos Mustad Serie 92247 (x50 unidades)</h5>
                 </div>
             </div>
             <!-- card herramienta5 -->
             <div class="col-sm-3">
-                <div class="shadow-sm text-center p-2 product-card">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
-                        <img src="{{ asset('img/catalogo/caja.jpg') }}" class="w-100 h-100 ">
+                <div class="text-center product-card-dark">
+                    <div class="mx-auto mb-1 overflow-hidden product-img" >
+                        <img src="{{ asset('img/catalogo/caja.jpg') }}" class="w-100 h-100 object-fit-cover">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$65.000</h4>
-                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0">Caja de Pesca Plano 3 Bandejas Articuladas</h5>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Caja de Pesca Plano 3 Bandejas Articuladas</h5>
                 </div>
             </div>
             <!-- card herramienta6 -->
             <div class="col-sm-3">
-                <div class="shadow-sm text-center p-2 product-card">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
-                        <img src="{{ asset('img/catalogo/pinza.jpg') }}" class="w-100 h-100 ">
+                <div class="text-center product-card-dark">
+                    <div class="mx-auto mb-1 overflow-hidden product-img" >
+                        <img src="{{ asset('img/catalogo/pinza.jpg') }}" class="w-100 h-100 object-fit-cover">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$22.000</h4>
-                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0">Pinza de Pesca Multifunción Spinit (Acero Inox)</h5>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Pinza de Pesca Multifunción Spinit (Acero Inox)</h5>
                 </div>
             </div>  
         </div>
@@ -160,11 +157,11 @@
     <section class=" my-5 ">
     <div class="container">
         <h2 class="text-center subtitulo-seccion fw-bold mb-5">Indumentaria</h2>
-        <div class="row row-cols-3 justify-content-evenly align-items-center g-4">
+        <div class="row justify-content-center align-items-center g-4">
             <!-- card indumentaria1 -->
             <div class="col-sm-3 p-2 p-md-3">
-                <div class="border shadow-sm text-center p-2 product-card">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
+                <div class="border text-center product-card">
+                    <div class="mx-auto mb-1 overflow-hidden product-img" >
                         <img src="{{ asset('img/catalogo/camisa.jpg') }}" class="w-100 h-100 object-fit-cover">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$110.000</h4>
@@ -173,9 +170,9 @@
             </div>
             <!-- card indumentaria2 -->
             <div class="col-sm-3">
-                <div class="border shadow-sm text-center p-2 product-card">
-                    <div class=" mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
-                        <img src="{{ asset('img/catalogo/wader.jpg') }}" class="w-100 h-100 ">
+                <div class="border text-center product-card">
+                    <div class=" mx-auto mb-1 overflow-hidden product-img" >
+                        <img src="{{ asset('img/catalogo/wader.jpg') }}" class="w-100 h-100 object-fit-cover">
                     </div>
                     <h4 class=" text-success fs-6 fs-sm-3 fw-bold mb-0">$350.000</h4>
                     <h5 class=" fw-semibold fs-6 fs-sm-3 mb-0">Wader de Neoprene Waterdog (4mm)</h5>
@@ -183,9 +180,9 @@
             </div>
             <!-- card indumentaria3 -->
             <div class="col-sm-3">
-                <div class="border shadow-sm text-center p-2 product-card ">
-                    <div class="mx-auto mb-1 overflow-hidden shadow-sm" style="width: 100%; height: 100%;">
-                        <img src="{{ asset('img/catalogo/bolso.jpg') }}" class="w-100 h-100 ">
+                <div class="border text-center product-card ">
+                    <div class="mx-auto mb-1 overflow-hidden product-img" >
+                        <img src="{{ asset('img/catalogo/bolso.jpg') }}" class="w-100 h-100 object-fit-cover">
                     </div>
                     <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$42.000</h4>
                     <h5 class="fw-semibold fs-6 fs-sm-3 mb-0">Bolso Estanco DryBag 20 Litros</h5>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,29 +3,110 @@
 
 @section('content')
 <!-- Carrousel de bienvenida -->
-  <div id="carouselExampleAutoplaying" class="carousel slide mx-auto" data-bs-ride="carousel" data-bs-interval="4000">
-    <div class="carousel-inner text-center">
-        <div class="carousel-item active">
-          <img src="{{ asset('img/banners/bann3.png')}}" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="{{ asset('img/banners/bann1.png')}}" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="{{ asset('img/banners/bann2.png')}}" class="d-block w-100" alt="...">
+<div id="carouselBienvenida" class="carousel slide mx-auto" data-bs-ride="carousel" data-bs-interval="4000">
+  <div class="carousel-inner text-center">
+    <div class="carousel-item active">
+      <img src="{{ asset('img/banners/bann3.png') }}" class="d-block w-100" alt="Banner 1">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('img/banners/bann1.png') }}" class="d-block w-100" alt="Banner 2">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('img/banners/bann2.png') }}" class="d-block w-100" alt="Banner 3">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselBienvenida" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselBienvenida" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!-- Fin Carrousel -->
+<!-- Productos Destacados -->
+<h2 class="text-center subtitulo-seccion fw-bold py-4">Productos Destacados</h2>
+<!-- Carrousel de Productos Destacados -->
+<div id="carouselProductosDestacados" class="carousel slide mx-auto mb-3" data-bs-ride="carousel" data-bs-interval="4000" >
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="row justify-content-center align-items-center g-4">
+            <!-- card articulo1 -->
+            <div class="col-sm-4">
+                <div class="border text-center product-card">
+                    <div class=" mx-auto mb-1 overflow-hidden product-img">
+                        <img src="{{ asset('img/catalogo/wader.jpg') }}" class="w-100 h-100 ">
+                    </div>
+                    <h4 class=" text-success fs-6 fs-sm-3 fw-bold mb-0">$350.000</h4>
+                    <h5 class=" fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Wader de Neoprene Waterdog (4mm)</h5>
+                </div>
+            </div>
+            <!-- card articulo2 -->
+            <div class="col-sm-4">
+                <div class="border text-center product-card">
+                    <div class="mx-auto mb-1 overflow-hidden product-img">
+                        <img src="{{ asset('img/catalogo/reel1.jpg') }}" class="w-100 h-100 ">
+                    </div>
+                    <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$85.000</h4>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Reel Frontal Daiwa Sweepfire 4000</h5>
+                </div>
+            </div>
+            <!-- card articulo3 -->
+            <div class="col-sm-4">
+                <div class="border text-center product-card ">
+                    <div class="mx-auto mb-1 overflow-hidden product-img">
+                        <img src="{{ asset('img/catalogo/cana1.jpg') }}" class="w-100 h-100 object-fit-cover">
+                    </div>
+                    <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$145.000</h4>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Caña Telescópica Shimano FX (3.00m)</h5>
+                </div>
+            </div>  
         </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div> 
-  <!-- Fin Carrousel -->
-  
+    <div class="carousel-item">
+      <div class="row justify-content-center align-items-center g-4">
+            <!-- card articulo4 -->
+            <div class="col-sm-4">
+                <div class="border text-center product-card">
+                    <div class="mx-auto mb-1 overflow-hidden product-img">
+                        <img src="{{ asset('img/catalogo/caja.jpg') }}" class="w-100 h-100 ">
+                    </div>
+                    <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$65.000</h4>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Caja de Pesca Plano 3 Bandejas Articuladas</h5>
+                </div>
+            </div>
+            <!-- card articulo5 -->
+            <div class="col-sm-4">
+                <div class="border text-center product-card">
+                    <div class="mx-auto mb-1 overflow-hidden product-img">
+                        <img src="{{ asset('img/catalogo/señuelo1.jpg') }}" class="w-100 h-100 object-fit-cover">
+                    </div>
+                    <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$32.000</h4>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Señuelo Rapala Super Shad Rap (14cm)</h5>
+                </div>
+            </div>
+            <!-- card articulo6 -->
+            <div class="col-sm-4">
+                <div class="border text-center product-card">
+                    <div class="mx-auto mb-1 overflow-hidden product-img">
+                        <img src="{{ asset('img/catalogo/reel3.jpg') }}" class="w-100 h-100 ">
+                    </div>
+                    <h4 class="text-success fs-6 fs-sm-3 fw-bold mb-0">$280.000</h4>
+                    <h5 class="fw-semibold fs-6 fs-sm-3 mb-0 pb-2">Reel Rotativo Penn Squall Level Wind</h5>
+                </div>
+            </div>  
+        </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselProductosDestacados" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselProductosDestacados" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!-- Fin Carrousel -->
 @endsection
-
-  
